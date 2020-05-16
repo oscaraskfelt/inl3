8280e51188f35b26dd5cd0b607e0e0cf2e9cc7de
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-16 13:36:18 +0200

    searchChar func added

--- js/heroAPI.js
+++ js/heroAPI.js
@@ -1,7 +1,12 @@
 const key = require('./key')
+const axios = require('axios')
 
 exports.getChar = async (id) => {
-    const request = await fetch(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${key.apiKey}/${id}/`)
-    const data = await request.json()
+    const data = await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${key.apiKey}/${id}/`)
     return data
-  }
+}
+
+exports.searchChar = async (name) => {
+    const data = await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${key.apiKey}/search/${name}/`)
+    return data
+}
