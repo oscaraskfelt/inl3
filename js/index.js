f2b8e96492c7d7c62c36cbb0acdafe302712c056
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-17 10:57:15 +0200

    creating chart in index.js + loading-span

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import *  as heroes from './heroAPI'
 import *  as create from './liCreator'
+import * as charts from './chartHandler'
+import Chart from 'chart.js'
 
 let chars = {
     faction1: null,
@@ -8,16 +10,38 @@ let chars = {
     char2: null
 }
 
+let chart;
+
 window.onload = () => {
     const form1 = document.getElementById('form1')
     const form2 = document.getElementById('form2')
     form1.addEventListener('submit', submitHandler)
     form2.addEventListener('submit', submitHandler)
+
+    const ctx = document.getElementById('chart').getContext('2d')
+    chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Intelligence', 'Combat', 'Durability'],
+            datasets: []
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }        
+    })
 }
 
 function submitHandler(e){
+    // TODO: add disabling submitbutton until call is complete
     e.preventDefault()
     const faction = this.id == 'form1' ? 1 : 0 
+    document.getElementById(`status${faction?1:2}`).innerHTML = "laddar.."
     const ul = document.getElementById(faction ? 'charPicker1' : 'charPicker2')
     const nameQuery = this.querySelector('.nameInput').value
     heroes.searchChar(nameQuery)
@@ -39,8 +63,13 @@ function submitHandler(e){
             else {
                 ul.innerHTML = 'No results'
             }
+            document.getElementById(`status${faction?1:2}`).innerHTML = ""
         }
     )
+    .catch(error => {
+        console.log(error)
+        document.getElementById(`status${faction?1:2}`).innerHTML = ""
+    })
 }
 
 
@@ -50,6 +79,9 @@ function charPicker(){
     ? factionNr = 1
     : factionNr = 2
     
-    chars[`char${factionNr}`] = chars[`faction${factionNr}`].filter(char => char.id == this.id)
+    chars[`char${factionNr}`] = chars[`faction${factionNr}`].filter(char => char.id == this.id)[0]
     document.getElementById(`charPicker${factionNr}`).innerHTML = ""
+
+    chart = charts.updateChart(chart, factionNr, chars[`char${factionNr}`])
+    chart.update()
 }
