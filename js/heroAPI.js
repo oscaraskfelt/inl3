1184534809fa5face0e3abb650ef89f6e14f331e
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-19 12:01:13 +0200

    remove unused func

--- js/heroAPI.js
+++ js/heroAPI.js
@@ -1,11 +1,6 @@
 const key = require('./key')
 const axios = require('axios')
 
-exports.getChar = async (id) => {
-    const data = await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${key.apiKey}/${id}/`)
-    return data
-}
-
 exports.searchChar = async (name) => {
     const data = await axios.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/${key.apiKey}/search/${name}/`)
     return data
